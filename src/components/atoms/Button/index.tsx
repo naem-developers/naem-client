1572d549eb9a76deef71a047933b821eb4adc3e8
Jeyoung-Park <jason9380@naver.com>
2022-07-14 22:07:29 +0900

    feat: Button 컴포넌트 제작

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { View, StyleSheet, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import Text from '@components/atoms/Text';
+import { THEME } from '@/constants';
+
+interface ButtonProps extends TouchableOpacityProps {
+  text: string;
+  priority?: 'primary' | 'secondary';
+  btnSize?: 'large' | 'small';
+  disabled?: boolean;
+}
+
+const Button = ({
+  text,
+  priority = 'primary',
+  btnSize = 'large',
+  disabled = false,
+  ...props
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[styles.container, styles[`${disabled ? 'disabled' : priority}Container`]]}
+      disabled={disabled}
+      {...props}
+    >
+      <Text style={{ ...styles.text, ...styles[`${disabled ? 'disabled' : priority}Text`] }}>
+        {text}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  primaryContainer: {
+    backgroundColor: THEME.MAIN,
+  },
+  secondaryContainer: {
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#c4c4c4',
+  },
+  disabledContainer: {
+    backgroundColor: '#f5f5f5',
+  },
+  largeContainer: {
+    alignSelf: 'stretch',
+    paddingVertical: 14,
+    borderRadius: 10,
+  },
+  smallContainer: {
+    paddingTop: 11,
+    paddingBottom: 8,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+  },
+  text: {
+    fontSize: 18,
+  },
+  primaryText: {
+    color: '#ffffff',
+  },
+  secondaryText: {
+    color: '#707070',
+  },
+  disabledText: {
+    color: '#A3A3A3',
+  },
+});
