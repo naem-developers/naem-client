769cf1070a34ecc8b3b776671c7d9709aaf4fd0c
Jeyoung-Park <jason9380@naver.com>
2022-07-15 19:16:23 +0900

    fix: header 파일 구조

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,15 +1,20 @@
 import Text from '@/components/atoms/Text';
+import { THEME } from '@/theme';
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 interface HeaderProps {
   title?: string;
+  LeftComponent?: React.ReactNode;
+  RightComponent?: React.ReactNode;
 }
 
-const Header = ({ title }: HeaderProps) => {
+const Header = ({ title, LeftComponent, RightComponent }: HeaderProps) => {
   return (
     <View style={styles.container}>
-      <Text>{title}</Text>
+      <View>{LeftComponent}</View>
+      <Text style={styles.title}>{title}</Text>
+      <View>{RightComponent}</View>
     </View>
   );
 };
@@ -17,5 +22,16 @@ const Header = ({ title }: HeaderProps) => {
 export default Header;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 14,
+    paddingBottom: 19,
+  },
+  title: {
+    fontSize: 18,
+    color: THEME.STRONG_TEXT,
+    fontWeight: '700',
+  },
 });
