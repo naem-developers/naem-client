483f652aa1efc906e1981b02fa43c5fbdaac5c00
Jeyoung-Park <jason9380@naver.com>
2022-07-16 23:20:36 +0900

    fix: text setter

--- src/pages/signup/signup/index.tsx
+++ src/pages/signup/signup/index.tsx
@@ -5,7 +5,7 @@ import { H_PADDING } from '@/constants';
 import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
 import { THEME } from '@/theme';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -13,6 +13,11 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 interface SignUpPageProps extends NativeStackScreenProps<SignUpStackParamList, 'SignUp'> {}
 
 const SignUpPage = ({ navigation }: SignUpPageProps) => {
+  const [phoneNum, setPhoneNum] = useState<string>('');
+  const [id, setId] = useState<string>('');
+  const [pw, setPw] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+
   return (
     <SafeAreaView style={styles.container}>
       <Header title="회원가입" />
@@ -25,6 +30,8 @@ const SignUpPage = ({ navigation }: SignUpPageProps) => {
             style={styles.input}
             placeholder="‘-’ 없이 입력해주세요"
             keyboardType="number-pad"
+            value={phoneNum}
+            onChangeText={(text) => setPhoneNum(text)}
           />
           <Button text="인증번호 발송" btnSize="small" style={styles.btn} />
         </View>
@@ -32,7 +39,12 @@ const SignUpPage = ({ navigation }: SignUpPageProps) => {
           아이디<Text style={styles.starSup}>*</Text>
         </Text>
         <View style={styles.row}>
-          <TextInput style={styles.input} placeholder="아이디를 입력해주세요" />
+          <TextInput
+            style={styles.input}
+            placeholder="아이디를 입력해주세요"
+            value={id}
+            onChangeText={(text) => setId(text)}
+          />
           <Button text="중복 확인" btnSize="small" style={styles.btn} />
         </View>
         <Text style={styles.subtitle}>
@@ -42,17 +54,25 @@ const SignUpPage = ({ navigation }: SignUpPageProps) => {
           style={[styles.input, styles.mt10]}
           placeholder="8~13자 이내 (영문, 숫자, 특수문자 포함)"
           secureTextEntry
+          value={pw}
+          onChangeText={(text) => setPw(text)}
         />
         <Text style={styles.subtitle}>
           닉네임<Text style={styles.starSup}>*</Text>
         </Text>
         <View style={styles.row}>
-          <TextInput style={styles.input} placeholder="5자 이내, 특수문자 불가" />
+          <TextInput
+            style={styles.input}
+            placeholder="5자 이내, 특수문자 불가"
+            value={nickname}
+            onChangeText={(text) => setNickname(text)}
+          />
           <Button text="중복 확인" btnSize="small" style={styles.btn} />
         </View>
         <Text style={styles.subtitle}>
           비밀번호<Text style={styles.starSup}>*</Text>
         </Text>
+        {/* TODO: 인증 여부 플래그 추가 */}
         <Button
           text="장애인 등록 인증하러 가기"
           priority="secondary"
@@ -61,6 +81,7 @@ const SignUpPage = ({ navigation }: SignUpPageProps) => {
             navigation.navigate('DisabilityCertificatePage');
           }}
         />
+        {/* TODO: disabled 여부 */}
         <Button text="완료" style={styles.ctaBtn} />
       </KeyboardAwareScrollView>
     </SafeAreaView>
