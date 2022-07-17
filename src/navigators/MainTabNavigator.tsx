3112ff5088b18c888704a06f7630cf12512b3e41
Jeyoung-Park <jason9380@naver.com>
2022-07-17 16:01:06 +0900

    fix: custom tabbar setup

--- src/navigators/MainTabNavigator.tsx
+++ src/navigators/MainTabNavigator.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BoardPage from '@pages/board';
 import WelfarePlacePage from '@pages/welfarePlace';
 import MyPage from '@pages/profile';
 import HomePage from '@pages/home';
+import { TouchableOpacity, View } from 'react-native';
+import Text from '@/components/atoms/Text';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export type MainTabParamList = {
   Home: undefined;
@@ -14,9 +17,79 @@ export type MainTabParamList = {
 
 const Tab = createBottomTabNavigator();
 
+const TabContainer = ({ state, descriptors, navigation }: BottomTabBarProps) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            // The `merge: true` option makes sure that the params inside the tab screen are preserved
+            // navigation.navigate({ name: route.name, merge: true });
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingBottom: insets.bottom,
+            }}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>{label as string}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
 const MainTabNavigator = () => {
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+      tabBar={(props) => <TabContainer {...props} />}
+    >
       <Tab.Screen name="Home" component={HomePage} />
       <Tab.Screen name="Board" component={BoardPage} />
       <Tab.Screen name="WelfarePlace" component={WelfarePlacePage} />
