7d7f8e53db18d2a4a3bde411531af9df3d2bb6ec
Jeyoung-Park <jason9380@naver.com>
2022-07-21 23:16:30 +0900

    feat: 장애인 등록증 확인 페이지 헤더 및 아이콘

--- src/pages/signup/disabilityCertificate/index.tsx
+++ src/pages/signup/disabilityCertificate/index.tsx
@@ -1,18 +1,31 @@
+import Header from '@/components/organisms/Header';
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import IcnCheck from '@/assets/icons/icn_check.svg';
+import { THEME } from '@/theme';
 
 interface DisabilityCertificatePageProps {}
 
 const DisabilityCertificatePage = (props: DisabilityCertificatePageProps) => {
   return (
-    <View style={styles.container}>
-      <Text>DisabilityCertificatePage</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header title="장애인 등록증 확인" />
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <IcnCheck width={54} height={54} />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default DisabilityCertificatePage;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1, backgroundColor: THEME.BG },
+  contentContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 24,
+    paddingBottom: 38,
+    alignItems: 'center',
+  },
 });
