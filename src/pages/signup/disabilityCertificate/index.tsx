f96f901a40a5fa91a584d19d7f5ec0d7f2bceb9d
Jeyoung-Park <jason9380@naver.com>
2022-07-22 00:46:56 +0900

    feat: 주의사항 및 서류 마크업

--- src/pages/signup/disabilityCertificate/index.tsx
+++ src/pages/signup/disabilityCertificate/index.tsx
@@ -6,6 +6,13 @@ import IcnCheck from '@/assets/icons/icn_check.svg';
 import { THEME } from '@/theme';
 import Text from '@/components/atoms/Text';
 
+const CAUTION_LIST = [
+  `•빛 반사가 되지 않도록 촬영해주세요.
+  어두운 바닥에서 촬영하면 잘 인식됩니다.`,
+  '•복사본이나 사진은 사용할 수 없습니다.',
+  '•장애인 등록증이 없는 경우 그 외 가능한 서류로 인증 가능합니다.',
+];
+
 interface DisabilityCertificatePageProps {}
 
 const DisabilityCertificatePage = (props: DisabilityCertificatePageProps) => {
@@ -24,6 +31,13 @@ const DisabilityCertificatePage = (props: DisabilityCertificatePageProps) => {
             source={require('@/assets/images/img_certificate_ex.png')}
           />
         </Pressable>
+        <Text style={styles.cautionTitle}>주의사항</Text>
+        {CAUTION_LIST.map((cautionText) => (
+          <Text style={styles.cautionText}>{cautionText}</Text>
+        ))}
+        <Text style={styles.otherDocText}>
+          <Text style={styles.otherDocStrongText}>그 외 가능한 서류</Text> 로 인증하기
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
@@ -58,5 +72,35 @@ const styles = StyleSheet.create({
   sampleImg: {
     width: '100%',
     height: '100%',
+  },
+  cautionTitle: {
+    fontSize: 15,
+    lineHeight: 22,
+    fontWeight: '700',
+    color: THEME.REG_TEXT,
+    marginTop: 30,
+    alignSelf: 'flex-start',
+  },
+  cautionText: {
+    marginTop: 10,
+    fontSize: 15,
+    lineHeight: 22,
+    fontWeight: '500',
+    color: THEME.LIGHT_TEXT,
+    alignSelf: 'flex-start',
+  },
+  otherDocText: {
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 22,
+    color: THEME.STRONG_TEXT,
+    marginTop: 30,
+  },
+  otherDocStrongText: {
+    fontSize: 16,
+    fontWeight: '700',
+    lineHeight: 22,
+    color: THEME.MAIN,
+    textDecorationLine: 'underline',
   },
 });
