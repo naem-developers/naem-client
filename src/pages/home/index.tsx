1174b8534fa3d6e809a1995d250d7b9a4671d69c
Jeyoung-Park <jason9380@naver.com>
2022-07-24 23:52:58 +0900

    feat: header 영역

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,32 @@
+import { THEME } from '@/theme';
 import * as React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, StyleSheet, View, Image } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface HomePageProps {}
 
 const HomePage = (props: HomePageProps) => {
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>HomePage</Text>
-    </SafeAreaView>
+    <View style={[styles.container]}>
+      <View style={[styles.headerContainer, { paddingTop: insets.top }]}>
+        <Image style={styles.catchPhrase} source={require('@/assets/images/img_catchphrase.png')} />
+      </View>
+    </View>
   );
 };
 
 export default HomePage;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1 },
+  headerContainer: {
+    backgroundColor: THEME.MAIN,
+    paddingHorizontal: 16,
+    paddingBottom: 30,
+  },
+  catchPhrase: {
+    width: 205,
+    height: 48,
+  },
 });
