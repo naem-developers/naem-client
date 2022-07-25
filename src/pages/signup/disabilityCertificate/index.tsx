3ab98003c5eabf75f9210d2f70d0c69dd4295fbc
Jeyoung-Park <jason9380@naver.com>
2022-07-25 16:18:28 +0900

    fix: text style 적용

--- src/pages/signup/disabilityCertificate/index.tsx
+++ src/pages/signup/disabilityCertificate/index.tsx
@@ -11,10 +11,10 @@ import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
 import ImagePicker from 'react-native-image-crop-picker';
 
 const CAUTION_LIST = [
-  `•빛 반사가 되지 않도록 촬영해주세요.
+  `• 빛 반사가 되지 않도록 촬영해주세요.
   어두운 바닥에서 촬영하면 잘 인식됩니다.`,
-  '•복사본이나 사진은 사용할 수 없습니다.',
-  '•장애인 등록증이 없는 경우 그 외 가능한 서류로 인증 가능합니다.',
+  '• 복사본이나 사진은 사용할 수 없습니다.',
+  '• 장애인 등록증이 없는 경우 그 외 가능한 서류로 인증 가능합니다.',
 ];
 
 interface DisabilityCertificatePageProps
@@ -35,11 +35,17 @@ const DisabilityCertificatePage = ({ navigation }: DisabilityCertificatePageProp
   return (
     <SafeAreaView style={styles.container}>
       <Header title="장애인 등록증 확인" />
-      <ScrollView contentContainerStyle={styles.contentContainer}>
+      <ScrollView
+        contentContainerStyle={styles.contentContainer}
+        showsVerticalScrollIndicator={false}
+      >
         <IcnCheck width={54} height={54} />
-        <Text style={styles.mainText}>
+        <Text sizeStyle="f18" weightStyle="semiBold" style={styles.mainText}>
           장애인 인증을 위해{'\n'}
-          <Text style={styles.mainColor}>장애인 등록증</Text>을 준비해주세요
+          <Text sizeStyle="f18" weightStyle="semiBold" style={styles.mainColor}>
+            장애인 등록증
+          </Text>
+          을 준비해주세요
         </Text>
         <Pressable style={styles.sampleImgBtn} onPress={handleOpenGallery}>
           <Image
@@ -47,14 +53,19 @@ const DisabilityCertificatePage = ({ navigation }: DisabilityCertificatePageProp
             source={require('@/assets/images/img_certificate_ex.png')}
           />
         </Pressable>
-        <Text style={styles.cautionTitle}>주의사항</Text>
+        <Text sizeStyle="f15" weightStyle="bold" style={styles.cautionTitle}>
+          주의사항
+        </Text>
         {CAUTION_LIST.map((cautionText) => (
-          <Text key={cautionText} style={styles.cautionText}>
+          <Text key={cautionText} sizeStyle="f15" weightStyle="medium" style={styles.cautionText}>
             {cautionText}
           </Text>
         ))}
-        <Text style={styles.otherDocText}>
-          <Text style={styles.otherDocStrongText}>그 외 가능한 서류</Text> 로 인증하기
+        <Text sizeStyle="f16" weightStyle="medium" style={styles.otherDocText}>
+          <Text sizeStyle="f16" weightStyle="bold" style={styles.otherDocStrongText}>
+            그 외 가능한 서류
+          </Text>{' '}
+          로 인증하기
         </Text>
         <View style={styles.CTABtnContainer}>
           <Button
@@ -81,9 +92,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   mainText: {
-    fontSize: 18,
-    fontWeight: '600',
-    lineHeight: 22,
     textAlign: 'center',
     color: THEME.STRONG_TEXT,
     marginTop: 16,
@@ -101,32 +109,20 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   cautionTitle: {
-    fontSize: 15,
-    lineHeight: 22,
-    fontWeight: '700',
     color: THEME.REG_TEXT,
     marginTop: 30,
     alignSelf: 'flex-start',
   },
   cautionText: {
     marginTop: 10,
-    fontSize: 15,
-    lineHeight: 22,
-    fontWeight: '500',
     color: THEME.LIGHT_TEXT,
     alignSelf: 'flex-start',
   },
   otherDocText: {
-    fontSize: 16,
-    fontWeight: '500',
-    lineHeight: 22,
     color: THEME.STRONG_TEXT,
     marginTop: 30,
   },
   otherDocStrongText: {
-    fontSize: 16,
-    fontWeight: '700',
-    lineHeight: 22,
     color: THEME.MAIN,
     textDecorationLine: 'underline',
   },
