56153f38fcf9e322523c76597688b39f6d3939e3
Jeyoung-Park <jason9380@naver.com>
2022-07-28 21:43:03 +0900

    feat: scrollview 구조

--- src/pages/profile/myPostComment/index.tsx
+++ src/pages/profile/myPostComment/index.tsx
@@ -1,18 +1,35 @@
+import Text from '@/components/atoms/Text';
+import Header from '@/components/organisms/Header';
+import { H_PADDING } from '@/constants';
+import { THEME } from '@/theme';
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface MyPostCommentPageProps {}
 
 const MyPostCommentPage = (props: MyPostCommentPageProps) => {
   return (
-    <View style={styles.container}>
-      <Text>MyPostCommentPage</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header title="내 게시글 / 댓글" />
+      <ScrollView
+        contentContainerStyle={styles.contentContainerStyle}
+        showsVerticalScrollIndicator={false}
+      >
+        <Text>MyPostCommentPage</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default MyPostCommentPage;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: THEME.BG,
+  },
+  contentContainerStyle: {
+    padding: H_PADDING,
+  },
 });
