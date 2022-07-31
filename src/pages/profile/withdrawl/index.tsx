d5e8a2c87f846fb360e48ae58503d0169284f677
Jeyoung-Park <jason9380@naver.com>
2022-07-31 21:30:55 +0900

    feat: 페이지 구조

--- src/pages/profile/withdrawl/index.tsx
+++ src/pages/profile/withdrawl/index.tsx
@@ -1,18 +1,26 @@
+import Header from '@/components/organisms/Header';
+import { H_PADDING } from '@/constants';
+import { THEME } from '@/theme';
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface WithdrawlPageProps {}
 
 const WithdrawlPage = (props: WithdrawlPageProps) => {
   return (
-    <View style={styles.container}>
-      <Text>WithdrawlPage</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header title="회원 탈퇴" />
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <Text>WithdrawlPage</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default WithdrawlPage;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1, backgroundColor: THEME.BG },
+  contentContainer: { paddingHorizontal: H_PADDING },
 });
