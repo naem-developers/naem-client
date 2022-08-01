8fb58eb608dccb32d5de2f5df102d543569f07f5
Jeyoung-Park <jason9380@naver.com>
2022-08-02 00:56:50 +0900

    feat: divider 추가

--- src/pages/welfarePlace/index.tsx
+++ src/pages/welfarePlace/index.tsx
@@ -3,20 +3,23 @@ import TextBtn from '@/components/atoms/TextBtn';
 import { THEME } from '@/theme';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Divider, Modal } from 'react-native-paper';
+import { Modal } from 'react-native-paper';
 import IcnExclamationMark from '@/assets/icons/icn_exclamation_mark.svg';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface WelfarePlacePageProps {}
 
 const WelfarePlacePage = (props: WelfarePlacePageProps) => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { paddingTop: insets.top }]}>
       <Modal visible contentContainerStyle={styles.contentContainer} dismissable={false}>
         <IcnExclamationMark width={60} height={60} />
         <Text sizeStyle="f17" weightStyle="semiBold" style={styles.text}>
           서비스 준비 중입니다
         </Text>
-        <View style={styles.divider} />
+        <View style={[styles.divider]} />
         <TextBtn text="확인" textStyle={styles.btnText} sizeStyle="f17" weightStyle="semiBold" />
       </Modal>
     </View>
@@ -31,20 +34,18 @@ const styles = StyleSheet.create({
     backgroundColor: THEME.BG,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center',
-    paddingHorizontal: 80,
+    marginHorizontal: 80,
     paddingTop: 24,
     paddingBottom: 20,
     borderRadius: 20,
-    marginHorizontal: 45,
   },
   text: { color: THEME.STRONG_TEXT, marginTop: 16 },
   divider: {
     height: 1,
-    backgroundColor: 'red',
-    alignSelf: 'stretch',
+    backgroundColor: THEME.LIGHT_LINE,
     marginTop: 20,
     marginBottom: 16,
+    width: '100%',
   },
   btnText: { color: THEME.REG_TEXT },
 });
