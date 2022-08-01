88439156be29e6860f9a4e05777024312e9c495e
Jeyoung-Park <jason9380@naver.com>
2022-08-02 00:32:33 +0900

    feat: 모달 텍스트

--- src/pages/welfarePlace/index.tsx
+++ src/pages/welfarePlace/index.tsx
@@ -1,19 +1,36 @@
-import * as React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import Text from '@/components/atoms/Text';
+import { THEME } from '@/theme';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Modal } from 'react-native-paper';
 
 interface WelfarePlacePageProps {}
 
 const WelfarePlacePage = (props: WelfarePlacePageProps) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>WelfarePlacePage</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Modal visible contentContainerStyle={styles.contentContainer} dismissable={false}>
+        <Text sizeStyle="f17" weightStyle="semiBold" style={styles.text}>
+          서비스 준비 중입니다
+        </Text>
+      </Modal>
+    </View>
   );
 };
 
 export default WelfarePlacePage;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1 },
+  contentContainer: {
+    backgroundColor: THEME.BG,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingHorizontal: 80,
+    paddingTop: 24,
+    paddingBottom: 20,
+    borderRadius: 20,
+  },
+  text: { color: THEME.STRONG_TEXT },
 });
