18510a1a9592ff9b0de642b02de9a277a53afd86
Jeyoung-Park <jason9380@naver.com>
2022-08-02 00:48:10 +0900

    fix: 서비스 준비중 다른 요소 추가

--- src/pages/welfarePlace/index.tsx
+++ src/pages/welfarePlace/index.tsx
@@ -1,8 +1,10 @@
 import Text from '@/components/atoms/Text';
+import TextBtn from '@/components/atoms/TextBtn';
 import { THEME } from '@/theme';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Modal } from 'react-native-paper';
+import { Divider, Modal } from 'react-native-paper';
+import IcnExclamationMark from '@/assets/icons/icn_exclamation_mark.svg';
 
 interface WelfarePlacePageProps {}
 
@@ -10,9 +12,12 @@ const WelfarePlacePage = (props: WelfarePlacePageProps) => {
   return (
     <View style={styles.container}>
       <Modal visible contentContainerStyle={styles.contentContainer} dismissable={false}>
+        <IcnExclamationMark width={60} height={60} />
         <Text sizeStyle="f17" weightStyle="semiBold" style={styles.text}>
           서비스 준비 중입니다
         </Text>
+        <View style={styles.divider} />
+        <TextBtn text="확인" textStyle={styles.btnText} sizeStyle="f17" weightStyle="semiBold" />
       </Modal>
     </View>
   );
@@ -31,6 +36,15 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingBottom: 20,
     borderRadius: 20,
+    marginHorizontal: 45,
   },
-  text: { color: THEME.STRONG_TEXT },
+  text: { color: THEME.STRONG_TEXT, marginTop: 16 },
+  divider: {
+    height: 1,
+    backgroundColor: 'red',
+    alignSelf: 'stretch',
+    marginTop: 20,
+    marginBottom: 16,
+  },
+  btnText: { color: THEME.REG_TEXT },
 });
