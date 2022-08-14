a8e96c2fe756d9dac04aa1b88de75650f81768b9
Jeyoung-Park <jason9380@naver.com>
2022-08-14 17:42:07 +0900

    chore: ios firebase setup

--- ios/NaemClient/AppDelegate.mm
+++ ios/NaemClient/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -30,6 +31,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
