2f2fe317b20323c9c69f7f0ed65d8a71f93c3b04
Jeyoung-Park <jason9380@naver.com>
2022-08-20 17:45:39 +0900

    Revert "chore: ios firebase setup"
    This reverts commit a8e96c2fe756d9dac04aa1b88de75650f81768b9.

--- ios/NaemClient/AppDelegate.mm
+++ ios/NaemClient/AppDelegate.mm
@@ -1,5 +1,4 @@
 #import "AppDelegate.h"
-#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -31,8 +30,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
-  
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
