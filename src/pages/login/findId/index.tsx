b0ae4e5d8517ee2c943ddf32c9696e3885597f38
Jeyoung-Park <jason9380@naver.com>
2022-08-24 00:45:11 +0900

    fix: 아이디 찾기 완료 뷰

--- src/pages/login/findId/index.tsx
+++ src/pages/login/findId/index.tsx
@@ -1,18 +1,56 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import IcnCheck from '@/assets/icons/icn_check.svg';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Text from '@/components/atoms/Text';
+import Header from '@/components/organisms/Header';
+import { H_PADDING } from '@/constants';
+import { THEME } from '@/theme';
 
 interface FindIdPageProps {}
 
 const FindIdPage = (props: FindIdPageProps) => {
   return (
-    <View style={styles.container}>
-      <Text>FindIdPage</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header title="아이디 찾기" />
+      <ScrollView contentContainerStyle={styles.contentContainerStyle}>
+        <IcnCheck width={53} height={53} />
+        <Text sizeStyle="f19" weightStyle="semiBold" style={styles.text}>
+          아이디 찾기 완료!
+        </Text>
+        <View style={styles.idContainer}>
+          <Text sizeStyle="f15" weightStyle="medium" style={styles.black}>
+            회원님의 아이디는 아래와 같습니다
+          </Text>
+          <Text sizeStyle="f20" weightStyle="semiBold" colorStyle="main" style={styles.mt12}>
+            naem12341234
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default FindIdPage;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1, backgroundColor: THEME.BG },
+  contentContainerStyle: {
+    paddingHorizontal: H_PADDING,
+    alignItems: 'center',
+    paddingTop: 135,
+    paddingBottom: 20,
+  },
+  text: { color: '#000000', marginTop: 24 },
+  black: { color: '#000000' },
+  idContainer: {
+    backgroundColor: THEME.LIGHT_GRAY,
+    padding: 24,
+    marginTop: 24,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  mt12: { marginTop: 12 },
 });
