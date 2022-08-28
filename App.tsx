d324073e99143fdd7d7dbe7fed398e44574c949b
Jeyoung-Park <jason9380@naver.com>
2022-08-28 18:09:15 +0900

    chore: install react-query & axios

--- App.tsx
+++ App.tsx
@@ -12,6 +12,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import RootStackNavigator from './src/navigators/RootStackNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   useEffect(() => {
@@ -20,11 +24,13 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <PaperProvider>
-        <NavigationContainer>
-          <RootStackNavigator />
-        </NavigationContainer>
-      </PaperProvider>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider>
+          <NavigationContainer>
+            <RootStackNavigator />
+          </NavigationContainer>
+        </PaperProvider>
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
