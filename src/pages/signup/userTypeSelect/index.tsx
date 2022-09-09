39e693e717c4a45d0a46a5e92ea08c98b805acbe
Jeyoung-Park <jason9380@naver.com>
2022-09-10 00:51:50 +0900

    feat: 선택 ui:

--- src/pages/signup/userTypeSelect/index.tsx
+++ src/pages/signup/userTypeSelect/index.tsx
@@ -2,9 +2,10 @@ import Text from '@/components/atoms/Text';
 import SignUpTemplate from '@/components/signup/signUpTemplate';
 import Title from '@/components/signup/title';
 import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
+import { THEME } from '@/theme';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Image, Pressable } from 'react-native';
 
 const CURRENT_STEP = 2;
 interface UserTypeSelectPageProps
@@ -17,6 +18,30 @@ const UserTypeSelectPage = (props: UserTypeSelectPageProps) => {
       <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt6}>
         가입 회원 유형을 선택해주세요
       </Text>
+      <View style={[styles.alignCenter, styles.mt28]}>
+        <Pressable style={styles.alignCenter}>
+          <View style={styles.typeContainer}>
+            <Image
+              style={styles.disabledImg}
+              source={require('@/assets/images/signup/disabled.png')}
+            />
+          </View>
+          <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt12}>
+            장애인 본인
+          </Text>
+        </Pressable>
+        <Pressable style={[styles.alignCenter, styles.mt60]}>
+          <View style={styles.typeContainer}>
+            <Image
+              style={styles.parentsImg}
+              source={require('@/assets/images/signup/parents.png')}
+            />
+          </View>
+          <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt12}>
+            장애인 본인
+          </Text>
+        </Pressable>
+      </View>
     </SignUpTemplate>
   );
 };
@@ -25,5 +50,21 @@ export default UserTypeSelectPage;
 
 const styles = StyleSheet.create({
   container: {},
+  typeContainer: {
+    width: 156,
+    height: 116,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // ...StyleSheet.absoluteFillObject,
+    borderRadius: 28,
+    borderColor: THEME.LIGHT_LINE,
+    borderWidth: 1.5,
+  },
+  disabledImg: { width: 42.51, height: 56.89 },
+  parentsImg: { width: 64, height: 42 },
+  alignCenter: { alignItems: 'center', marginTop: 28 },
   mt6: { marginTop: 6 },
+  mt12: { marginTop: 12 },
+  mt28: { marginTop: 28 },
+  mt60: { marginTop: 60 },
 });
