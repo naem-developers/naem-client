658176f62afde87c5c506bb5178bd1833bbe6d72
Jeyoung-Park <jason9380@naver.com>
2022-09-09 23:52:25 +0900

    feat: signUp Title 컴포넌트 및 회원 유형 컴포틑

--- src/pages/signup/userTypeSelect/index.tsx
+++ src/pages/signup/userTypeSelect/index.tsx
@@ -1,17 +1,22 @@
 import Text from '@/components/atoms/Text';
 import SignUpTemplate from '@/components/signup/signUpTemplate';
+import Title from '@/components/signup/title';
 import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
+const CURRENT_STEP = 2;
 interface UserTypeSelectPageProps
   extends NativeStackScreenProps<SignUpStackParamList, 'UserTypeSelectPage'> {}
 
 const UserTypeSelectPage = (props: UserTypeSelectPageProps) => {
   return (
-    <SignUpTemplate currentStep={2}>
-      <Text>가입 회원 유형을 선택해주세요</Text>
+    <SignUpTemplate currentStep={CURRENT_STEP}>
+      <Title step={CURRENT_STEP} text="회원 유형" />
+      <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt6}>
+        가입 회원 유형을 선택해주세요
+      </Text>
     </SignUpTemplate>
   );
 };
@@ -20,4 +25,5 @@ export default UserTypeSelectPage;
 
 const styles = StyleSheet.create({
   container: {},
+  mt6: { marginTop: 6 },
 });
