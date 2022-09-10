36c70dbe0a815d54ee3d18b3e5ae48fb18dff108
Jeyoung-Park <jason9380@naver.com>
2022-09-10 19:55:42 +0900

    feat: 장애인 인증 ui

--- src/pages/signup/disabled/indext.tsx
+++ src/pages/signup/disabled/indext.tsx
@@ -3,13 +3,15 @@ import Text from '@/components/atoms/Text';
 import TextInput from '@/components/atoms/TextInput';
 import SignUpTemplate from '@/components/signup/signUpTemplate';
 import Title from '@/components/signup/title';
+import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
 import { validateNickname } from '@/utils/validation';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useCallback, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
-interface DisabledPageProps {}
+interface DisabledPageProps extends NativeStackScreenProps<SignUpStackParamList, 'DisabledPage'> {}
 
-const DisabledPage = (props: DisabledPageProps) => {
+const DisabledPage = ({ navigation }: DisabledPageProps) => {
   const [inputHeight, setInputHeight] = useState<number | undefined>();
   const [nickname, setNickname] = useState<string>('');
   const [nicknameValidationMsg, setNicknameValidationMsg] = useState<string>('');
@@ -45,6 +47,24 @@ const DisabledPage = (props: DisabledPageProps) => {
           onPress={checkNickname}
         />
       </View>
+      <Title step={4} text="장애인 인증" style={styles.mt62} />
+      <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt6}>
+        장애인 등록증 혹은 기타 서류가 필요합니다
+      </Text>
+      <View>
+        <Image
+          style={styles.sampleImg}
+          source={require('@/assets/images/img_certificate_ex.png')}
+        />
+      </View>
+      <Button
+        text="장애인 인증하러 가기"
+        priority="secondary"
+        style={styles.mt16}
+        onPress={() => {
+          navigation.navigate('DisabilityCertificatePage');
+        }}
+      />
     </SignUpTemplate>
   );
 };
@@ -54,5 +74,8 @@ export default DisabledPage;
 const styles = StyleSheet.create({
   inputContainer: { flexDirection: 'row', alignItems: 'flex-start', marginTop: 10 },
   duplicateBtn: { marginLeft: 12 },
+  sampleImg: { height: 208, aspectRatio: 1.75, marginTop: 10 },
   mt6: { marginTop: 6 },
+  mt16: { marginTop: 16 },
+  mt62: { marginTop: 62 },
 });
