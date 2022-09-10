a3a1e1bd49d890919d2f2057ddaee8ebc177fb47
Jeyoung-Park <jason9380@naver.com>
2022-09-10 20:23:29 +0900

    fix: 닉네임 가입 텍스트 수정

--- src/pages/signup/disabled/indext.tsx
+++ src/pages/signup/disabled/indext.tsx
@@ -31,9 +31,9 @@ const DisabledPage = ({ navigation }: DisabledPageProps) => {
       currentStep={3}
       btnProps={{ onPress: handlePressNext, disabled: nickname?.length === 0 || !checkNickname }}
     >
-      <Title step={3} text="회원 유형" />
+      <Title step={3} text="닉네임" />
       <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt6}>
-        가입 회원 유형을 선택해주세요
+        나음에서 사용할 닉네임을 조건에 맞게 입력해주세요
       </Text>
       <View style={styles.inputContainer}>
         <TextInput
