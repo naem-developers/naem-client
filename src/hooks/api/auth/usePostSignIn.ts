9a3425a3b6b96b32a0c6ef2304e718a1641789a0
Jeyoung-Park <jason9380@naver.com>
2022-09-12 22:02:40 +0900

    feat: encrytedStorage에 token 정보 저장

--- src/hooks/api/auth/usePostSignIn.ts
+++ src/hooks/api/auth/usePostSignIn.ts
@@ -3,6 +3,8 @@ import { IReqSignIn } from '@/api/types/request';
 import { applyToken } from '@/utils/auth';
 import { useMutation } from '@tanstack/react-query';
 import Toast from 'react-native-toast-message';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import { USER_STORAGE_KEY } from '@/constants/storageKeys';
 
 export default () => {
   const mutation = async (body: IReqSignIn) => {
@@ -14,6 +16,10 @@ export default () => {
     onSuccess: (res) => {
       Toast.show({ type: 'success', text1: '로그인에 성공하였습니다.' });
       applyToken(res.data.access_token);
+      EncryptedStorage.setItem(
+        USER_STORAGE_KEY,
+        JSON.stringify({ accessToken: res.data.access_token, refreshToken: res.data.refresh }),
+      );
     },
     onError: (e) => {
       Toast.show({ type: 'error', text1: '로그인에 실패하였습니다.' });
