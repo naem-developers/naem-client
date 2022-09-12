98009cfd1bdc311b25d643aa329b3e420153209f
Jeyoung-Park <jason9380@naver.com>
2022-09-12 22:09:53 +0900

    feat: clearToken 시 encryptedStorage 비우기

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -1,4 +1,6 @@
 import { client } from '@/api';
+import { USER_STORAGE_KEY } from '@/constants/storageKeys';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 export const applyToken = (jwt: string) => {
   client.defaults.headers.common.Authorization = `Bearer ${jwt}`;
@@ -6,4 +8,5 @@ export const applyToken = (jwt: string) => {
 
 export const clearToken = () => {
   delete client.defaults.headers.common['Authorization'];
+  EncryptedStorage.removeItem(USER_STORAGE_KEY);
 };
