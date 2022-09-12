c731f5ec773946e7786027b2aad7c2e9f7b4432b
Jeyoung-Park <jason9380@naver.com>
2022-09-12 21:32:21 +0900

    feat: applytoken, cleartoken 함수 적용

--- src/hooks/api/auth/usePostSignIn.ts
+++ src/hooks/api/auth/usePostSignIn.ts
@@ -1,5 +1,6 @@
 import API from '@/api';
 import { IReqSignIn } from '@/api/types/request';
+import { applyToken } from '@/utils/auth';
 import { useMutation } from '@tanstack/react-query';
 import Toast from 'react-native-toast-message';
 
@@ -12,7 +13,7 @@ export default () => {
   return useMutation(mutation, {
     onSuccess: (res) => {
       Toast.show({ type: 'success', text1: '로그인에 성공하였습니다.' });
-      console.log('res, ', res);
+      applyToken(res.data.access_token);
     },
     onError: (e) => {
       Toast.show({ type: 'error', text1: '로그인에 실패하였습니다.' });
