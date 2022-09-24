09e9f689dfa06f29200966b6e78fb4802efd59a6
Jeyoung-Park <jason9380@naver.com>
2022-09-24 17:54:30 +0900

    chore: asyncstorage 추가

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
-import EncryptedStorage from 'react-native-encrypted-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-Reactotron.setAsyncStorageHandler(EncryptedStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
   .configure() // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
