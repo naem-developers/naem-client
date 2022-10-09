a920d9bc2602f0a86762844e77e0b6ec258562e5
Jeyoung-Park <jason9380@naver.com>
2022-10-10 01:26:37 +0900

    feat: 체크박스 ui 구조

--- src/pages/signup/terms/index.tsx
+++ src/pages/signup/terms/index.tsx
@@ -7,15 +7,26 @@ import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
 import { THEME } from '@/theme';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { Pressable, StyleSheet, useWindowDimensions } from 'react-native';
+import { Pressable, StyleSheet, useWindowDimensions, View } from 'react-native';
 import AutoHeightWebView from 'react-native-autoheight-webview';
 import { Checkbox } from 'react-native-paper';
+
+const SERVICE_TERM_LIST = {
+  SERVICE_OVERVIEW: '서비스 개요',
+  SIGN_UP_PROCESS: '회원 가입 절차',
+  COMMUNITY_RULE: '커뮤니티 이용 수칙',
+  SERVICE_WITHDRAWL_POLICY: '서비스 탈퇴 정책',
+};
+
+const PERSONAL_INFORMATION = {
+  PERSONAL_INFO_POLICY: '개인정보 처리 방침',
+  FEEDBACK: '피드백 및 소통',
+  NOTICE: '약관 및 운영정책 공지',
+};
 
 interface TermsPageProps extends NativeStackScreenProps<SignUpStackParamList, 'TermsPage'> {}
 
 const TermsPage = ({ navigation, route }: TermsPageProps) => {
-  // const [allSel]
-
   return (
     <SignUpTemplate
       currentStep={1}
@@ -34,10 +45,56 @@ const TermsPage = ({ navigation, route }: TermsPageProps) => {
           // status={checkedIdxList.length === CHECK_LIST.length ? 'checked' : 'unchecked'}
           uncheckedColor="#c9c9c9"
         />
-        <Text sizeStyle="f14" weightStyle="medium" style={styles.checkAllText}>
-          모두 확인했습니다
+        <Text sizeStyle="f14" weightStyle="medium" colorStyle='strongText'>
+          모두 동의
         </Text>
       </Pressable>
+      <View style={styles.checkBoxContainer}>
+        <View style={styles.row}>
+          <Checkbox.Android
+            status="checked"
+            // status={checkedIdxList.length === CHECK_LIST.length ? 'checked' : 'unchecked'}
+            uncheckedColor="#c9c9c9"
+          />
+          <Text sizeStyle="f14" weightStyle="medium" colorStyle='lightText'>
+            이용약관 >
+          </Text>
+        </View>
+        {Object.values(SERVICE_TERM_LIST).map((item, index)=>{
+          return <Pressable key={`${item}-${index}`} style={[styles.row, styles.ml20]}>
+          <Checkbox.Android
+            status="checked"
+            // status={checkedIdxList.length === CHECK_LIST.length ? 'checked' : 'unchecked'}
+            uncheckedColor="#c9c9c9"
+          />
+          <Text sizeStyle="f14" weightStyle="medium" colorStyle='lightText'>
+            {item}
+          </Text>
+        </Pressable>
+        })}
+        <View style={[styles.row, styles.mt28]}>
+          <Checkbox.Android
+            status="checked"
+            // status={checkedIdxList.length === CHECK_LIST.length ? 'checked' : 'unchecked'}
+            uncheckedColor="#c9c9c9"
+          />
+          <Text sizeStyle="f14" weightStyle="medium" colorStyle='lightText'>
+            개인정보 처리 및 소통 >
+          </Text>
+        </View>
+        {Object.values(PERSONAL_INFORMATION).map((item, index)=>{
+          return <Pressable key={`${item}-${index}`} style={[styles.row, styles.ml20]}>
+          <Checkbox.Android
+            status="checked"
+            // status={checkedIdxList.length === CHECK_LIST.length ? 'checked' : 'unchecked'}
+            uncheckedColor="#c9c9c9"
+          />
+          <Text sizeStyle="f14" weightStyle="medium" colorStyle='lightText'>
+            {item}
+          </Text>
+        </Pressable>
+        })}
+      </View>
       {/* <AutoHeightWebView
         containerStyle={{ width: width - H_PADDING * 2 }}
         source={{ html: SERVICE_TERMS_HTML }}
@@ -49,7 +106,15 @@ const TermsPage = ({ navigation, route }: TermsPageProps) => {
 export default TermsPage;
 
 const styles = StyleSheet.create({
-  mt6: { marginTop: 6 },
   checkAllContainer: { flexDirection: 'row', alignItems: 'center', marginTop: 10 },
-  checkAllText: { color: THEME.LIGHT_TEXT },
+  checkBoxContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 18,
+    backgroundColor: THEME.LIGHT_GRAY,
+    borderRadius: 10,
+  },
+  row:{flexDirection:'row', alignItems:'center'},
+  mt6: { marginTop: 6 },
+  mt28:{marginTop:28},
+  ml20:{marginLeft:20}
 });
