9532ccec0198f374ec03b0d44a8c8ce8a34e31d2
Jeyoung-Park <jason9380@naver.com>
2022-10-09 17:38:43 +0900

    feat: 약관 페이지 상단 텍스트

--- src/pages/signup/terms/index.tsx
+++ src/pages/signup/terms/index.tsx
@@ -1,18 +1,20 @@
 import Text from '@/components/atoms/Text';
 import SignUpTemplate from '@/components/signup/signUpTemplate';
+import Title from '@/components/signup/title';
 import { H_PADDING } from '@/constants';
 import { SERVICE_TERMS_HTML } from '@/constants/terms';
 import { SignUpStackParamList } from '@/navigators/SignUpStackNavigator';
 import { THEME } from '@/theme';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { StyleSheet, useWindowDimensions } from 'react-native';
+import { Pressable, StyleSheet, useWindowDimensions } from 'react-native';
 import AutoHeightWebView from 'react-native-autoheight-webview';
+import { Checkbox } from 'react-native-paper';
 
 interface TermsPageProps extends NativeStackScreenProps<SignUpStackParamList, 'TermsPage'> {}
 
 const TermsPage = ({ navigation, route }: TermsPageProps) => {
-  const { width } = useWindowDimensions();
+  // const [allSel]
 
   return (
     <SignUpTemplate
@@ -22,12 +24,32 @@ const TermsPage = ({ navigation, route }: TermsPageProps) => {
           navigation.navigate('UserTypeSelectPage', { loginInfo: route.params.loginInfo }),
       }}
     >
-      <AutoHeightWebView
+      <Title step={1} text="이용약관" />
+      <Text sizeStyle="f14" weightStyle="medium" colorStyle="regText" style={styles.mt6}>
+        회원가입 전 이용약관에 동의해주세요
+      </Text>
+      <Pressable style={styles.checkAllContainer}>
+        <Checkbox.Android
+          status="checked"
+          // status={checkedIdxList.length === CHECK_LIST.length ? 'checked' : 'unchecked'}
+          uncheckedColor="#c9c9c9"
+        />
+        <Text sizeStyle="f14" weightStyle="medium" style={styles.checkAllText}>
+          모두 확인했습니다
+        </Text>
+      </Pressable>
+      {/* <AutoHeightWebView
         containerStyle={{ width: width - H_PADDING * 2 }}
         source={{ html: SERVICE_TERMS_HTML }}
-      />
+      /> */}
     </SignUpTemplate>
   );
 };
 
 export default TermsPage;
+
+const styles = StyleSheet.create({
+  mt6: { marginTop: 6 },
+  checkAllContainer: { flexDirection: 'row', alignItems: 'center', marginTop: 10 },
+  checkAllText: { color: THEME.LIGHT_TEXT },
+});
